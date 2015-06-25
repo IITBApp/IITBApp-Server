3b660352865c5ce6af70157510632c9235c94bbd
Rathor <dheerenr@amazon.com>
2015-06-26 00:07:06 +0530

    a desperate attempt to work with heroku

--- iitbapp/urls.py
+++ iitbapp/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,3 +25,9 @@ urlpatterns = [
     url(r'', include(authentication_urls)),
     url(r'', include(news_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+        }),
+    )
