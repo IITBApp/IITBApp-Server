e901738cc9032ad121d5cb918135445e0c377ba7
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-07-24 15:00:44 +0530

    changed gunicorn logging level to info

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 pkill gunicorn
-nohup gunicorn iitbapp.wsgi:application --timeout 600 --workers 10 --log-level=debug --reload --bind=0.0.0.0:9090
+nohup gunicorn iitbapp.wsgi:application --timeout 600 --workers 10 --log-level=info --reload --bind=0.0.0.0:9090
