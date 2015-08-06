a5b6456bf69772013d0c00558b7a86866ef97ba8
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-07 02:41:25 +0530

    Removed total_views from events due to bugs

--- event/admin.py
+++ event/admin.py
@@ -4,12 +4,12 @@ from django.db.models import Count, Sum
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'posted_by', 'total_likes', 'unique_views', 'total_views']
+    #TODO: Add total views if possible in 1 or 2 queries. Adding Sum in queryset gives error
+    list_display = ['id', 'title', 'category', 'posted_by', 'total_likes', 'unique_views']
 
     def get_queryset(self, request):
         queryset = Event.objects.all().annotate(total_likes=Count('likes', distinct=True),
-                                                unique_views=Count('views', distinct=True),
-                                                total_views=Sum('views__view_count'))
+                                                unique_views=Count('views', distinct=True))
         return queryset
 
     def total_likes(self, ins):
@@ -17,9 +17,6 @@ class EventAdmin(admin.ModelAdmin):
 
     def unique_views(self, ins):
         return ins.unique_views
-
-    def total_views(self, ins):
-        return ins.total_views
 
 
 class EventImageAdmin(admin.ModelAdmin):
