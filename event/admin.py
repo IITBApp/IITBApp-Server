10db7c690a900f94bd72c33d5b2a4dc11cf641ef
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-06 20:56:44 +0530

    Added likes, views infographics in event

--- event/admin.py
+++ event/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from models import Event, EventImage, EventLike, EventViews
+from django.db.models import Count, Sum
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'posted_by']
+    list_display = ['id', 'title', 'category', 'posted_by', 'total_likes', 'unique_views', 'total_views']
+
+    def get_queryset(self, request):
+        return Event.objects.all().annotate(total_likes=Count('likes'), unique_views=Count('views'),
+                                            total_views=Sum('views__view_count'))
+
+    def total_likes(self, ins):
+        return ins.total_likes
+
+    def unique_views(self, ins):
+        return ins.unique_views
+
+    def total_views(self, ins):
+        return ins.total_views
 
 
 class EventImageAdmin(admin.ModelAdmin):
@@ -16,6 +30,7 @@ class EventViewAdmin(admin.ModelAdmin):
 
 class EventLikeAdmin(admin.ModelAdmin):
     list_display = ['id', 'event', 'user']
+
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventImage, EventImageAdmin)
