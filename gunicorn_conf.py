e99d678f84f673c4688d3044e7c037268e85b480
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-21 21:56:43 +0530

    Added X-Real-IP in gunicorn

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -9,5 +9,5 @@ def max_workers():
 workers = max_workers()
 worker_class = 'gevent'
 accesslog = "logs/gunicorn_access.log"
-access_log_format = "%(h)s %(D)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
+access_log_format = "%(h)s %({X-Real-IP}i)s %(D)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
 errorlog = "logs/gunicorn_error.log"
