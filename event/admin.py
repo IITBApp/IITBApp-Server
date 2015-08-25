7398fce07234a27c8ae0e2039e6a530812054723
Dheerendra Rathor <dheeru.rathor14@gmail.com>
2015-08-25 06:15:52 +0530

    Testing total_view on prod

--- event/admin.py
+++ event/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from models import Event, EventImage, EventLike, EventViews
-from django.db.models import Prefetch
+from django.db.models import Prefetch, Sum
 
 class EventAdmin(admin.ModelAdmin):
     # TODO: Add total views if possible in 1 or 2 queries. Adding Sum in queryset gives error
-    list_display = ['id', 'title', 'category', 'posted_by', 'total_likes', 'unique_views']
+    list_display = ['id', 'title', 'category', 'posted_by', 'total_likes', 'unique_views', 'total_views']
 
     def get_queryset(self, request):
         queryset = Event.objects.all().prefetch_related(Prefetch('likes', EventLike.objects.all())).prefetch_related(
@@ -16,6 +16,11 @@ class EventAdmin(admin.ModelAdmin):
 
     def unique_views(self, ins):
         return ins.views.count()
+
+    def total_views(self, ins):
+        queryset = ins.views.all()
+        queryset = queryset.aggregate(Sum('view_count'))
+        return queryset.get('view_count__sum', 0)
 
 
 class EventImageAdmin(admin.ModelAdmin):
